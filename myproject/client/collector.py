b7071932e9187107d641eaeec6e3b9527a7bfaca
David Harrod <60739989+davidharrod@users.noreply.github.com>
2021-03-16 14:25:27 +0800

    Upload my project

---
+++ myproject/client/collector.py
@@ -0,0 +1,15 @@
+#收集操作系统的名称、版本
+#收集编译器的名称、版本
+#收集主机。。。
+import platform as pf
+
+def getSysInfo():
+    dic = {}
+    dic['os'] = pf.platform()
+    dic['compiler'] = pf.python_compiler()
+    dic['cpuArch'] = pf.processor()
+    return dic
+
+
+
+
